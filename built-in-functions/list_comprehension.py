1f8332c44498ffef09cc5d4c170d78c985df4194
Hugo Souto <hugomsouto@gmail.com>
2023-09-16 09:57:41 -0300

    Update file

--- built-in-functions/list_comprehension.py
+++ built-in-functions/list_comprehension.py
@@ -9,11 +9,22 @@
 # List comprehension is a way to create a list using a for loop in one line.
 
 # 1. Faster For Loops
+
 fruits = ['apples', 'bananas', 'strawberries']
 
-# Using a for loop
+# Print using a for loop
 for fruit in fruits:
     print(fruit)
 
-# Using list comprehension
+# Print using list comprehension
 [print(fruit) for fruit in fruits]
+
+# Change to upper case using a for loop
+for fruit in fruits:
+    fruits[fruits.index(fruit)] = str(fruit).upper()
+
+print(fruits)
+
+# Change to upper case using a list comprehension
+fruits_lc = [str(fruit).upper() for fruit in fruits]
+print(fruits_lc)
